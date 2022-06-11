44bf918788650d819a131cee50bf87ba515dce3f
SIB-BYTE <97762248+SIB-BYTE@users.noreply.github.com>
2022-06-11 03:53:23 +0000

    Update server.h

--- src/server.h
+++ src/server.h
@@ -26,26 +26,11 @@
 #define MAX_EVENTS 100
 #define set_nonblocking(fd) fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK);
 
-enum user_state {
-	USER_OFFLINE,
-	USER_ONLINE,
-	USER_IDLE
-};
-
-typedef struct users {
-	char *username;
-	int time_active, user_state;
-} user_t;
-
 // Socket setup:
 int setup_socket(int, char *, short);
 
 // Data stream handling:
 void handle_data(int);
-void handle_disconnection(int);
-
-// Epoll user creation:
-void register_user(int, user_t *);
 
 // Epoll sheet:
 void setup_epoll(int);
